24243f0bca81fd2a293048236a0cb647c71b06eb
zitataylor <trendlockeronline@gmail.com>
2026-05-03 15:03:43 -0400

    Update index.js

--- api/index.js
+++ api/index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+app.use('/api/webhook', express.raw({type: 'application/json'}));
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
